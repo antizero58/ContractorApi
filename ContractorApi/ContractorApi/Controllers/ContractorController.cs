6cd9329c808f8d7187f21ea23d4aead210777756
AntiZero <AntiZero91@gmail.com>
2019-12-04 20:39:05 +0300

    Initial

---
+++ ContractorApi/ContractorApi/Controllers/ContractorController.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using ContractorApi.Models;
+
+namespace ContractorApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ContractorController : ControllerBase
+    {
+        private readonly ILogger<ContractorController> _logger;
+
+        static private readonly List<Contractor> _contractors = new List<Contractor>();
+
+        static ContractorController()
+        {
+            _contractors = Enumerable.Range(1, 5).Select(index => new Contractor
+            {
+                Id = index,
+                Name = "Name_" + index
+            })
+            .ToList<Contractor>();
+        }
+
+        public ContractorController(ILogger<ContractorController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<Contractor> Get()
+        {
+            return _contractors;
+        }
+
+        [HttpGet("{id}")]
+        public Contractor Get(int id)
+        {
+            if (_contractors.Exists(r => r.Id == id))
+                return null;
+
+            return _contractors.FirstOrDefault(r => r.Id == id);
+        }
+
+        [HttpPost]
+        public ActionResult Create(Contractor item)
+        {
+            if (item == null)
+                return BadRequest();
+
+            _contractors.Add(item);
+            return CreatedAtAction(nameof(Create), new { id = item.Id }, item);
+        }
+    }
+}
---
+++ ContractorApi/ContractorApi/Models/Contractor.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ContractorApi.Models
+{
+    public enum ContractorType
+    {
+        // Юр. лицо
+        LegalEntity,
+
+        // ИП
+        SoleProprietor
+    }
+
+    public class Contractor
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string FullName { get; set; }
+        public ContractorType Type { get; set; }
+        public string inn { get; set; }
+        public string kpp { get; set; }
+    }
+}
